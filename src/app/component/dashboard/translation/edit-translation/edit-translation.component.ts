2694051483b6873a6f1328e8be5251cdaba28993
Uros Maksimovic <maksimovic.u@yahoo.com>
2022-09-02 16:35:05 +0200

    add search bar for languages

--- src/app/component/dashboard/translation/edit-translation/edit-translation.component.ts
+++ src/app/component/dashboard/translation/edit-translation/edit-translation.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from "@angular/core";
 import { TranslationModel } from "src/app/models/translation-model";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
-import { DashboardService } from 'src/app/service/dashboard.service';
+import { DashboardService } from "src/app/service/dashboard.service";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-edit",
   templateUrl: "./edit-translation.component.html",
-  styleUrls: ["./edit-translation.component.scss"]
+  styleUrls: ["./edit-translation.component.scss"],
 })
 export class EditTranslationComponent implements OnInit {
   public schema: any;
@@ -17,7 +17,6 @@ export class EditTranslationComponent implements OnInit {
   public id: any;
   public loading = true;
   public language: any;
-  toExpand: boolean = false;
 
   constructor(
     private service: DashboardService,
@@ -31,69 +30,123 @@ export class EditTranslationComponent implements OnInit {
     this.language = JSON.parse(localStorage.getItem("language"));
     this.initialization();
   }
-  
-  initialization() {
-    
-    if(this.toExpand) {
-      this.service.getGridConfigurationScheme("translation").subscribe((data: any) => {
-        data.layout.map((el: any) => {
-          el.expanded = true;
-        })
-        this.schema = data;
-        this.loading = false;
+
+  camelize(str) {
+    return str
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+      })
+      .replace(/\s+/g, "");
+  }
+
+  searchSchema(event: any) {
+    let layout: any;
+    let layoutItems: any = [];
+    let items = {};
+    let inputValue = event;
+    let flag: boolean = false;
+    layout = this.schema.layout;
+
+    layout.map((el: any) => {
+      layoutItems.push(el.items);
+    });
+
+    layoutItems.map((el: any, index: number) => {
+      let currentLayoutItem = [];
+      el.map((currentEl: any) => {
+        currentLayoutItem.push(currentEl.items);
       });
-  
-    }else {
-      this.service.getGridConfigurationScheme("translation").subscribe((data: any) => {
-        this.schema = data;
-        this.loading = false;
+      items[index] = currentLayoutItem;
+    });
+
+    Object.keys(items).forEach((key, index) => {
+      items[key].map((currentEl: any) => {
+        currentEl.map((currentItems: any) => {
+          if (
+            currentItems["key"] == this.camelize(inputValue) ||
+            currentItems["key"] == inputValue
+          ) {
+            this.initialization(index);
+            flag = true;
+          }
+        });
       });
+    });
+    if (!flag) {
+      this.initialization();
     }
-    
+  }
+
+  initialization(indexToExpand?: number) {
+    if (indexToExpand > -1) {
+      this.service
+        .getGridConfigurationScheme("translation")
+        .subscribe((data: any) => {
+          data.layout[indexToExpand].expanded = true;
+          this.schema = data;
+          this.loading = false;
+        });
+    } else {
+      this.service
+        .getGridConfigurationScheme("translation")
+        .subscribe((data: any) => {
+          this.schema = data;
+          this.loading = false;
+        });
+    }
+
     if (this.id !== "create") {
-      this.service.getTranslationWithId(this.id).subscribe(data => {
+      this.service.getTranslationWithId(this.id).subscribe((data) => {
         this.data = data;
         this.loading = false;
       });
     }
   }
 
-  expandAll(): void {
-    this.toExpand = !this.toExpand
-    this.initialization();
-  }
-
   saveConfiguration(event) {
     this.data.config = event;
-    console.log(this.data);
     if (this.id === "create") {
-      this.service.createTranslation(this.data).subscribe(data => {
-        console.log(data);
+      this.service.createTranslation(this.data).subscribe((data) => {
         if (data) {
-          this.toastr.success(this.language.adminSuccessCreateTitle, this.language.adminSuccessCreateText, {
-            timeOut: 7000,
-            positionClass: "toast-bottom-right"
-          });
+          this.toastr.success(
+            this.language.adminSuccessCreateTitle,
+            this.language.adminSuccessCreateText,
+            {
+              timeOut: 7000,
+              positionClass: "toast-bottom-right",
+            }
+          );
         } else {
-          this.toastr.error(this.language.adminErrorCreateTitle, this.language.adminErrorCreateText, {
-            timeOut: 7000,
-            positionClass: "toast-bottom-right"
-          });
+          this.toastr.error(
+            this.language.adminErrorCreateTitle,
+            this.language.adminErrorCreateText,
+            {
+              timeOut: 7000,
+              positionClass: "toast-bottom-right",
+            }
+          );
         }
       });
     } else {
-      this.service.updateTranslation(this.data).subscribe(data => {
-        console.log(data);
+      this.service.updateTranslation(this.data).subscribe((data) => {
         if (data) {
-          this.toastr.success(this.language.adminSuccessUpdateTitle, this.language.adminSuccessUpdateText, {
-            timeOut: 7000,
-            positionClass: "toast-bottom-right"
-          });
+          this.toastr.success(
+            this.language.adminSuccessUpdateTitle,
+            this.language.adminSuccessUpdateText,
+            {
+              timeOut: 7000,
+              positionClass: "toast-bottom-right",
+            }
+          );
         } else {
-          this.toastr.error(this.language.adminErrorUpdateTitle, this.language.adminErrorUpdateText, {
-            timeOut: 7000,
-            positionClass: "toast-bottom-right"
-          });
+          this.toastr.error(
+            this.language.adminErrorUpdateTitle,
+            this.language.adminErrorUpdateText,
+            {
+              timeOut: 7000,
+              positionClass: "toast-bottom-right",
+            }
+          );
         }
       });
     }
