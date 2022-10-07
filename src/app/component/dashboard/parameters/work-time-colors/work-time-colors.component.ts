db19fc1c73720052bbeaf988c0c75189e61272d4
Uros Maksimovic <83064029+MaksimovicUros@users.noreply.github.com>
2022-10-07 13:32:56 +0200

    Cnum 1209 (#103)
    * small fix
    
    * bug fix
    
    * add dynamic translations for dynamic grid
    
    * bug fix pagination
    
    * change register clinics template id

--- src/app/component/dashboard/parameters/work-time-colors/work-time-colors.component.ts
+++ src/app/component/dashboard/parameters/work-time-colors/work-time-colors.component.ts
@@ -87,10 +87,17 @@ export class WorkTimeColorsComponent implements OnInit {
 
     this.getWorkTimeColors();
 
+    this.setPagination();
+  }
+
+  setPagination() {
     this.savePage = this.helpService.getGridPageSize();
-    if(this.savePage && this.savePage[this.currentUrl] || this.savePage[this.currentUrl + 'Take']) {
+    if (
+      (this.savePage && this.savePage[this.currentUrl]) ||
+      this.savePage[this.currentUrl + "Take"]
+    ) {
       this.state.skip = this.savePage[this.currentUrl];
-      this.state.take = this.savePage[this.currentUrl + 'Take'];
+      this.state.take = this.savePage[this.currentUrl + "Take"];
     }
   }
   
@@ -105,6 +112,9 @@ export class WorkTimeColorsComponent implements OnInit {
       .getWorkTimeColors(localStorage.getItem("superadmin"))
       .subscribe((data: []) => {
         this.currentLoadData = data;
+        if(this.currentLoadData.length < this.state.skip) {
+          this.state.skip = 0;
+        }
         this._allData = <ExcelExportData>{
           data: process(this.currentLoadData, this.state).data,
         }
